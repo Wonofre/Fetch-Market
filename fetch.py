41ab05080688d66499d99567be2a49a4a2948985
Wonofre <133052084+Wonofre@users.noreply.github.com>
2023-11-19 15:16:22 -0300

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify, request, Response
 import yfinance as yf
-from datetime import datetime, timedelta
+from datetime import datetime
 import os
 import pandas as pd
 
 app = Flask(__name__)
 
-def fetch_historical_data(symbol):
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=30)  # Last month
+def fetch_historical_data(symbol, start_date, end_date):
     ticker = yf.Ticker(symbol)
     historical_data = ticker.history(start=start_date, end=end_date)
     return historical_data
@@ -16,22 +14,25 @@ def fetch_historical_data(symbol):
 @app.route('/historical_data', methods=['GET'])
 def get_historical_data():
     symbol = request.args.get('symbol', default='AAPL', type=str)
-    data = fetch_historical_data(symbol)
+    start_date = request.args.get('start', default=(datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d'), type=str)
+    end_date = request.args.get('end', default=datetime.now().strftime('%Y-%m-%d'), type=str)
+    data = fetch_historical_data(symbol, start_date, end_date)
     return jsonify(data.to_json())
 
 @app.route('/historical_data_csv', methods=['GET'])
 def get_historical_data_csv():
     symbol = request.args.get('symbol', default='AAPL', type=str)
-    data = fetch_historical_data(symbol)
-    # Convert DataFrame to CSV
+    start_date = request.args.get('start', default=(datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d'), type=str)
+    end_date = request.args.get('end', default=datetime.now().strftime('%Y-%m-%d'), type=str)
+    data = fetch_historical_data(symbol, start_date, end_date)
     csv_data = data.to_csv(index=False)
     return Response(csv_data, mimetype='text/csv', headers={"Content-disposition": f"attachment; filename={symbol}_historical_data.csv"})
 
 @app.route('/')
 def home():
-    return "Welcome to the Stock Data API! Use /historical_data?symbol=SYMBOL to fetch data in JSON format, or /historical_data_csv?symbol=SYMBOL to fetch data in CSV format."
+    return "Welcome to the Stock Data API! Use /historical_data?symbol=SYMBOL&start=START_DATE&end=END_DATE to fetch data in JSON format, or /historical_data_csv?symbol=SYMBOL&start=START_DATE&end=END_DATE to fetch data in CSV format."
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Heroku will supply the PORT environment variable
-    app.run(host='0.0.0.0', port=port, debug=False)  # Turn off debug mode when deploying to production
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
